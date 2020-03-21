e7816643eaaa3292d6ba7a7be59078d019e37392
glebrctu <gleb172v@gmail.com>
2020-03-21 16:09:03 +0300

    update

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-import time
 from opcua import Server
 from random import randint
 from Initialization_Parametrs import Parametrs
@@ -21,27 +20,23 @@ class OPCServer:
             variable.append(self.Object_node.add_variable(self.addspace, f'{parameters.name}', 0))
         return variable
 
-
     def set_value(self, variables: list, list_parameters: list):
         i = 0
         for var in variables:
             var.set_writable()
             var.set_value(randint(list_parameters[i].restriction[0], list_parameters[i].restriction[1]))
             i += 1
-        return variables
 
     def run(self):
         self.server.start()
         print(f"Server start at {self.url}")
 
+    def generate_data(self, parameters: list):
+        while True:
+            list_variables = opc.set_variable(Parametrs)
+            opc.set_value(list_variables, Parametrs)
+
 
 opc = OPCServer()
 opc.run()
-
-while True:
-    list_variables = opc.set_variable(Parametrs)
-    value = opc.set_value(list_variables, Parametrs)
-    for v in value:
-        print(v.get_value())
-    #time.sleep(1)
-
+opc.generate_data(Parametrs)
