40afd27f98ad48998493b6c9a900cf3a079b2016
danildanil86660 <dan0712@yandex.ru>
2020-03-21 15:37:27 +0300

    Update clientDB Add function: - create_bd - create_table

--- clientDB.py
+++ clientDB.py
@@ -2,3 +2,24 @@ from clickhouse_driver.client import Client
 
 client = Client('localhost')
 print(client.execute('SHOW TABLE'))
+
+
+class ClientDB:
+
+    def __int__(self, url_server_click_haus='localhost'):
+        self.clien = Client(url_server_click_haus)
+        self.list_parameters = {'Pressure': 'Float64',
+                                'Humidity': 'Float64',
+                                'Area_temperature': 'Float64',
+                                'Work_temperature': 'Float64',
+                                'pH_level': 'Float64',
+                                'Weight': 'Float64',
+                                'Fluid_flow': 'Float64',
+                                'CO2_flow': 'Float64',
+                                }
+
+    def create_bd(self, name_bd: str):
+        self.clien.execute(f'CREATE DATABASE IF NOT EXISTS {name_bd}')
+
+    def create_table(self):
+        pass
