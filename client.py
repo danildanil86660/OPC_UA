a55fa6ea6313b53a7c04bfb572c41cef600595a4
danildanil86660 <dan0712@yandex.ru>
2020-03-21 03:54:39 +0300

    Runing client

--- client.py
+++ client.py
@@ -1,24 +1,24 @@
 from opcua import Client
 import time
 
-url = "opt.tcp://192.168.1.187:4840"
+url = "opc.tcp://127.0.0.1:48402/freeopcua/server/"
 
 client = Client(url)
-
 client.connect()
 print('Client connected')
 
 while True:
     Temp = client.get_node('ns=2; i=2')
     Temperature = Temp.get_value()
-    print(Temperature)
 
     Press = client.get_node('ns=2; i=3')
     Pressure = Press.get_value()
-    print(Pressure)
 
     Time = client.get_node('ns=2; i=4')
     Time_value = Time.get_value()
-    print(Time_value)
+
+    Flow = client.get_node('ns=2; i=5')
+    Flow_value = Flow.get_value()
+    print(Time_value, Pressure, Temperature, Flow_value)
 
     time.sleep(1)
--- server.py
+++ server.py
@@ -1,14 +1,13 @@
 import time
-
 from opcua import Server
 from random import randint
 import datetime
 
 server = Server()
 
-url = "opt.tcp://192.168.1.187:4840"
+url = "opc.tcp://127.0.0.1:48402/freeopcua/server/"
 server.set_endpoint(url)
-
+#server.set_application_uri(url)
 name = "OPCUA_SIMULATION_SERVER"
 addspace = server.register_namespace(name)
 
@@ -18,22 +17,24 @@ Param = node.add_object(addspace, "Parameters")
 
 Temp = Param.add_variable(addspace, 'Temperature', 0)
 Press = Param.add_variable(addspace, 'Pressure', 0)
+Flow = Param.add_variable(addspace, 'Flow', 0)
 Time = Param.add_variable(addspace, 'Time', 0)
 
 Temp.set_writable()
 Press.set_writable()
 Time.set_writable()
-
+Flow.set_writable()
+server.start()
 print(f"Server start at {url}")
 while True:
     Temperature = randint(10, 50)
     Pressure = randint(100, 999)
     TIME = datetime.datetime.now()
-
-    print(TIME, Temperature, Pressure)
+    FLOW = randint(50, 60)
 
     Temp.set_value(Temperature)
     Press.set_value(Pressure)
-    Time.set_writable(TIME)
+    Time.set_value(TIME)
+    Flow.set_value(FLOW)
 
-    time.sleep(2)
+    time.sleep(1)
