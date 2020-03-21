1fc092891df7eced2ac43ac3aecb0169f2f53558
danildanil86660 <dan0712@yandex.ru>
2020-03-21 19:04:17 +0300

    Frist realise

--- clientDB.py
+++ clientDB.py
@@ -1,25 +1,22 @@
 from clickhouse_driver.client import Client
 
-client = Client('localhost')
-print(client.execute('SHOW TABLE'))
-
 
 class ClientDB:
 
-    def __int__(self, url_server_click_haus='localhost'):
-        self.clien = Client(url_server_click_haus)
+    def __init__(self, url_server_click_haus='localhost'):
+        self.client = Client(url_server_click_haus)
         self.list_fields = {'Pressure': 'Float64',
-                                'Humidity': 'Float64',
-                                'Area_temperature': 'Float64',
-                                'Work_temperature': 'Float64',
-                                'pH_level': 'Float64',
-                                'Weight': 'Float64',
-                                'Fluid_flow': 'Float64',
-                                'CO2_flow': 'Float64',
-                                }
+                            'Humidity': 'Float64',
+                            'Area_temperature': 'Float64',
+                            'Work_temperature': 'Float64',
+                            'pH_level': 'Float64',
+                            'Weight': 'Float64',
+                            'Fluid_flow': 'Float64',
+                            'CO2_flow': 'Float64',
+                            }
 
     def create_bd(self, name_bd: str):
-        self.clien.execute(f'CREATE DATABASE IF NOT EXISTS {name_bd}')
+        self.client.execute(f'CREATE DATABASE IF NOT EXISTS {name_bd}')
 
     def create_table(self):
         tmp = list()
@@ -28,5 +25,16 @@ class ClientDB:
 
         field_table = ', '.join(i for i in tmp)
         sql = f'CREATE TABLE test_table({field_table})'
-        self.clien.execute(sql)
-        pass
+        self.client.execute(sql)
+
+    def test(self):
+        return self.client.execute('SELECT 1')
+
+
+def main():
+    b = ClientDB()
+    print(b.test())
+
+
+if __name__ == '__main__':
+    main()
