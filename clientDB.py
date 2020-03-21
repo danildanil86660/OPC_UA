64665e54b813f64fafc7f0feffc7ef4f625b2df6
danildanil86660 <dan0712@yandex.ru>
2020-03-21 16:47:34 +0300

    Rafactor name files and update clientDB

--- clientDB.py
+++ clientDB.py
@@ -8,7 +8,7 @@ class ClientDB:
 
     def __int__(self, url_server_click_haus='localhost'):
         self.clien = Client(url_server_click_haus)
-        self.list_parameters = {'Pressure': 'Float64',
+        self.list_fields = {'Pressure': 'Float64',
                                 'Humidity': 'Float64',
                                 'Area_temperature': 'Float64',
                                 'Work_temperature': 'Float64',
@@ -22,4 +22,11 @@ class ClientDB:
         self.clien.execute(f'CREATE DATABASE IF NOT EXISTS {name_bd}')
 
     def create_table(self):
+        tmp = list()
+        for field in self.list_fields:
+            tmp.clear()
+            tmp.append(":".join(i for i in [field, self.list_fields[field]]))
+
+        field_table = ','.join(i for i in tmp)
+        sql = f'CREATE TABLE test_table()'
         pass
