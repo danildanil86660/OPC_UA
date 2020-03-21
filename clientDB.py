e8a36536df4c71a43e0874fcc4142fc18cb6c131
danildanil86660 <dan0712@yandex.ru>
2020-03-21 13:58:02 +0300

    Update clientDB

--- clientDB.py
+++ clientDB.py
@@ -1,4 +1,4 @@
 from clickhouse_driver.client import Client
 
-client = Client('192.168.56.1:8123')
-print(client.execute('select 1'))
+client = Client('localhost')
+print(client.execute('SHOW TABLE'))
